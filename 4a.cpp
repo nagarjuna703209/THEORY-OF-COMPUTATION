d46745edbbc8738fddf28673c2c504a452c3b5d1
nagarjuna703209 <113428311+nagarjuna703209@users.noreply.github.com>
2023-08-07 08:46:51 +0530

    Add files via upload

---
+++ 4a.cpp
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+	int s[20],index[19],i;
+	printf("enter the string:");
+	scanf("%s",&s);
+	
+	for(i=0;i<s[20];i++)
+	{
+		if(index[0]= 0)
+		{
+			printf("valid");
+			
+		}
+		else 
+		{
+			printf("invalid");
+		}
+	}
+	return 0;
+}
